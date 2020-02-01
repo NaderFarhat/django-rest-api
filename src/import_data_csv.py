e404ab9c43136a0f25ab94e81af8c0f85aa23407
Nader Farhat <n-farhat@hotmail.com>
2020-02-01 19:17:21 -0300

    Mudanças arquivo import csv

--- src/import_data_csv.py
+++ src/import_data_csv.py
@@ -1,18 +1,17 @@
 """
-    Script to import book data from .csv file to Model Database DJango
-    To execute this script run: 
-                                1) manage.py shell
-                                2) exec(open('import_data_csv.py').read())
+    1) manage.py shell
+    2) exec(open('import_data_csv.py').read())
 """
 
 import csv
 from events.models import Comitees, Event
 
-CSV_PATH = '../noc_regions.csv'      # Csv file path 
-CSV_PATH_EVENT = '../athlete_events.csv'      # Csv file path   
+CSV_PATH = '../noc_regions.csv' 
+CSV_PATH_EVENT = '../athlete_events.csv' 
 
 contSuccess_noc = 0
-# Remove all data from Table
+contSuccess_evt = 0
+
 Comitees.objects.all().delete()
 Event.objects.all().delete()
 
@@ -20,12 +19,17 @@ with open(CSV_PATH, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     print('Loading...')
     for row in spamreader:
-        Comitees.objects.create(
-            noc=row[0],
-            region=row[1],
-            notes=row[2]
-        )
-        contSuccess_noc += 1
+        if row:
+            if row[0] == 'NOC':
+                pass
+            else:
+                print(row[0])
+                Comitees.objects.create(
+                    noc=row[0],
+                    region=row[1],
+                    notes=row[2]
+                )
+                contSuccess_noc += 1
     print(f'{str(contSuccess_noc)} inserted successfully (NOC)! ')
 
 with open(CSV_PATH_EVENT, newline='') as f:
@@ -34,25 +38,11 @@ with open(CSV_PATH_EVENT, newline='') as f:
     for row in reader:
         if row:
             filterNoc = Comitees.objects.filter(noc=row[7]).values('noc', 'region', 'notes').first()
-            #N = Noc.objects.create(noc=filterNoc['noc'], region=filterNoc['region'], notes=['notes'])
+            if filterNoc is None:
+                filterNoc = {'noc': ' ', 'region': ' ', 'notes': ' '}
             if row[0] == 'ID':
                 pass
             else:
-                print(row[0])
-                print(row[1])
-                print(row[2])
-                print(row[3])
-                print(row[4])
-                print(row[5])
-                print(row[6])
-                print(row[7])
-                print(row[8])
-                print(row[9])
-                print(row[10])
-                print(row[11])
-                print(row[12])
-                print(row[13])
-                print(row[14])
                 Event.objects.create(
                     identification=row[0],
                     name=row[1],
@@ -70,6 +60,7 @@ with open(CSV_PATH_EVENT, newline='') as f:
                     event=row[13],
                     medal=row[14]
                 )
-
+                contSuccess_evt += 1
+    print(f'{str(contSuccess_evt)} inserted successfully (EVT)! ')
 
 
