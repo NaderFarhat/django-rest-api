8349a8955a316999ac09f16170a6942f08fafa8f
Nader Farhat <n-farhat@hotmail.com>
2020-02-01 10:09:50 -0300

    Adicionando Modelos

--- src/events/api/tests.py
+++ src/events/api/tests.py
@@ -3,31 +3,42 @@ from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
 from rest_framework import status
 from rest_framework.reverse import reverse as api_reverse
+from events.models import Event, Comitees
 
 
 
 class CRUDTestCase(APITestCase):
 
-    def test_registration_noc(self):
-        data = {
-            "noc": "AHO",
-            "region": "Curacao",
-            "notes": "Netherlands Antilles"
-        }
-        response = self.client.post('/api/events/nocs/', data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_registration_athletes(self):
-        data = {
-        }
-
-        response = self.client.post('/api/events/teste/', data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def setUp(self):
+        noc_obj = Comitees(        
+            noc= "AHO",
+            region= "Curacao",
+            notes= "Netherlands Antilles"
+        )
+        noc_obj.save()
+        evt_obj = Event(
+            identification= 5,
+            name= "Christine Jacoba Aaftink",
+            sex= "F",
+            age= 32,
+            height= 182,
+            weight= 60,
+            team= "Netherlands",
+            noc= Comitees.objects.first() ,
+            games= "1992 Summer",
+            year= 1992,
+            season= "Winter",
+            city= "Barcelona",
+            sport= "Basketball",
+            event= "Basketball Men's Basketball",
+            medal= "Gold"
+        )
+        evt_obj.save()
         
     def test_only_one_noc(self):
-        noc_count = Noc.objects.count()
+        noc_count = Comitees.objects.count()
         self.assertEqual(noc_count, 1)
 
     def test_only_one_athlete(self):
-        events_count = Events.objects.count()
+        events_count = Event.objects.count()
         self.assertEqual(events_count, 1)
