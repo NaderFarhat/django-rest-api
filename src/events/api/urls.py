1b3bca5033b82b500e0bec4edb331d111cccfc94
Nader Farhat <n-farhat@hotmail.com>
2020-02-01 21:11:32 -0300

    alterações views

--- src/events/api/urls.py
+++ src/events/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 
-from .views import EventAPIView, EventRudView, EventFilterView, ComiteeAPIView, ComiteeRudView, ComiteesFilterView
+from .views import EventAPIView, EventRudView, ComiteeAPIView, ComiteeRudView
 
 app_name = 'myapi'
 
 urlpatterns = [
-    #path('evt/', EventAPIView.as_view(), name='event-create'),
-    path('evt/', EventFilterView.as_view(), name='event-filter'),
+    path('evt/', EventAPIView.as_view(), name='event-list'),
+    #path('evt/', EventFilterView.as_view(), name='event-filter'),
     path('evt/<int:pk>/', EventRudView.as_view(), name='event-detail'),
-    #path('com/', ComiteeAPIView.as_view(), name='comitee-list'),
-    path('com/', ComiteesFilterView.as_view(), name='comitee-filter'),
+    path('com/', ComiteeAPIView.as_view(), name='comitee-list'),
+    #path('com/', ComiteesFilterView.as_view(), name='comitee-filter'),
     path('com/<int:pk>/', ComiteeRudView.as_view(), name='comitee-detail'),
 ]
--- src/events/api/views.py
+++ src/events/api/views.py
@@ -3,19 +3,15 @@ from events.models import Event, Comitees
 from .serializers import EventsSerializer, ComiteesSerializer
 from django_filters import rest_framework as filters
 
-class EventFilterView(generics.ListCreateAPIView):
-    queryset = Event.objects.all()
+class EventAPIView(generics.ListCreateAPIView):
+    lookup_field = 'pk'
     serializer_class = EventsSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = '__all__'
 
-class EventAPIView(generics.CreateAPIView):
-    lookup_field = 'pk'
-    serializer_class = EventsSerializer
-
     def get_queryset(self):
-        return Event.objects.all()
-
+        return Event.objects.all()[:50]
+    
 
 class EventRudView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
@@ -24,26 +20,17 @@ class EventRudView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Event.objects.all()
 
-
-class ComiteesFilterView(generics.ListCreateAPIView):
-    queryset = Comitees.objects.all()
+class ComiteeAPIView(generics.ListCreateAPIView):
+    lookup_field = 'pk'
     serializer_class = ComiteesSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = '__all__'
 
-
-class ComiteeAPIView(generics.CreateAPIView):
-    lookup_field = 'pk'
-    serializer_class = ComiteesSerializer
-
     def get_queryset(self):
-        return Comitees.objects.all()
-
+        return Comitees.objects.all()[:50]
 
 class ComiteeRudView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
     serializer_class = ComiteesSerializer
-
-    def get_queryset(self):
-        return Comitees.objects.all()
+    queryset = Comitees.objects.all()
         
