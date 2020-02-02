094d3cdc073bb44cf46fec53f67817557d85f65d
Nader Farhat <n-farhat@hotmail.com>
2020-02-02 12:47:14 -0300

    mudança Modelo

--- src/events/api/urls.py
+++ src/events/api/urls.py
@@ -6,9 +6,7 @@ app_name = 'myapi'
 
 urlpatterns = [
     path('evt/', EventAPIView.as_view(), name='event-list'),
-    #path('evt/', EventFilterView.as_view(), name='event-filter'),
     path('evt/<int:pk>/', EventRudView.as_view(), name='event-detail'),
     path('com/', ComiteeAPIView.as_view(), name='comitee-list'),
-    #path('com/', ComiteesFilterView.as_view(), name='comitee-filter'),
     path('com/<int:pk>/', ComiteeRudView.as_view(), name='comitee-detail'),
 ]
